9b5b312744b06461b0f0787ee160a76ef3141124
Marko Hoffmann <mhoffmann@madomadu.de>
2018-06-04 12:54:05 +0000

    Add new file

---
+++ assets/pdnsmanager/config-user.php
@@ -0,0 +1,7 @@
+<?php
+$config['db_host'] = 'db';
+$config['db_user'] = 'root';
+$config['db_password'] = 'Nusodu78!';
+$config['db_name'] = 'pdns';
+$config['db_port'] = 3306;
+$config['db_type'] = 'mysql';
